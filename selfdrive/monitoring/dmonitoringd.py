6e0ef637f4c607399b7772fb70589d3eb1eea854
Sainisumit327 <57571437+Sainisumit327@users.noreply.github.com>
2025-09-27 14:54:20 +0530

    changes made

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import time
 import cereal.messaging as messaging
 from openpilot.common.params import Params
 from openpilot.common.realtime import config_realtime_process
@@ -7,27 +6,48 @@ from openpilot.selfdrive.monitoring.helpers import DriverMonitoring
 
 
 def dmonitoringd_thread():
-    config_realtime_process([0, 1, 2, 3], 5)
+  # keep realtime config so process manager doesn’t complain
+  config_realtime_process([0, 1, 2, 3], 5)
 
-    pm = messaging.PubMaster(['driverMonitoringState'])
-    params = Params()
+  params = Params()
+  pm = messaging.PubMaster(['driverMonitoringState'])
+  sm = messaging.SubMaster(['driverStateV2', 'liveCalibration', 'carState', 'selfdriveState', 'modelV2',
+                            'carControl'], poll='driverStateV2')
 
-    while True:
-        dat = messaging.new_message('driverMonitoringState')
-        dat.driverMonitoringState.faceDetected = True
-        dat.driverMonitoringState.isActiveMode = True
-        dat.driverMonitoringState.awake = True
-        dat.driverMonitoringState.faceProbability = 1.0
-        dat.driverMonitoringState.readyToDrive = True
+  # keep object init (unused now, just for stability)
+  DM = DriverMonitoring(rhd_saved=params.get_bool("IsRhdDetected"), always_on=params.get_bool("AlwaysOnDM"))
 
-        pm.send('driverMonitoringState', dat)
+  while True:
+    sm.update()
 
-        time.sleep(0.05)  # ~20 Hz loop
+    # build new message
+    dat = messaging.new_message('driverMonitoringState')
+    dms = dat.driverMonitoringState
+
+    # simulate attentive driver
+    dms.faceDetected = True
+    dms.isDistracted = False
+    dms.distractedType = 0
+    dms.awarenessStatus = 1.0
+    dms.posePitchOffset = 0.0
+    dms.posePitchValidCount = 1
+    dms.poseYawOffset = 0.0
+    dms.poseYawValidCount = 1
+    dms.stepChange = 0.0
+    dms.awarenessActive = 1.0
+    dms.awarenessPassive = 1.0
+    dms.isLowStd = False
+    dms.hiStdCount = 0
+    dms.isActiveMode = True
+    dms.isRHD = False
+
+    # publish
+    pm.send('driverMonitoringState', dat)
 
 
 def main():
-    dmonitoringd_thread()
+  dmonitoringd_thread()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
